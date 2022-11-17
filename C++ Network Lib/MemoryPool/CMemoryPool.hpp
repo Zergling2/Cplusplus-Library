d4967303d471abc91fd8bd67add7dd9c22546d01
upzerg <upzerg@naver.com>
2022-11-17 15:35:09 +0900

    C++ Lib commit

--- C++ Network Lib/MemoryPool/CMemoryPool.hpp
+++ C++ Network Lib/MemoryPool/CMemoryPool.hpp
@@ -2,10 +2,13 @@
 
 #pragma warning(disable:26495)
 
+#define _WINSOCKAPI_
 #include <Windows.h>
 #include <strsafe.h>
 #include "CLogger.h"
 #include "ForceCrash.h"
+
+class CSession;
 
 namespace SJNET
 {
@@ -17,17 +20,20 @@ namespace SJNET
 		private:
 			struct Node;
 		public:
-			inline CMemoryPool(DWORD flOptions, SIZE_T dwInitialSize, SIZE_T dwMaximumSize);	// (경고!) dwMaximumSize가 0이 아닌 경우 LFH 적용 불가 및 해당 힙에서의 객체 최대 할당 크기는 0x7FFF8로 제한됨.
-			~CMemoryPool();
-			template<typename... Types> ObjectType* GetObjectFromPool(Types ...args);
+			static CMemoryPool* GetInstance();
+			static void Destroy();
+			template<typename ...Types> ObjectType* GetObjectFromPool(Types ...args);
 			void ReturnObjectToPool(ObjectType* _Ret);
 		private:
+			CMemoryPool(DWORD flOptions, SIZE_T dwInitialSize, SIZE_T dwMaximumSize);	// (경고!) dwMaximumSize가 0이 아닌 경우 LFH 적용 불가 및 해당 힙에서의 객체 최대 할당 크기는 0x7FFF8로 제한됨.
+			~CMemoryPool();
 			static void GetObjectErrorCrash();
-			static void ReturnObjectErrorCrash(ULONG_PTR wrongVerificationCode);
+			static void ReturnObjectErrorCrash(ULONG_PTR llWrongVerificationCode);
 			Node* _Top;
-			inline static ULONG_PTR _VerificationCode = -1;
-			inline static HANDLE _HeapHandle = NULL;
-			inline static DWORD _HeapOptions;
+			static CMemoryPool<ObjectType>* pInstance;
+			static ULONG_PTR _VerificationCode;
+			static HANDLE _HeapHandle;
+			static DWORD _HeapOptions;
 		private:
 			struct Node
 			{
@@ -42,7 +48,26 @@ namespace SJNET
 		};
 
 		template<typename ObjectType>
-		inline CMemoryPool<ObjectType>::CMemoryPool(DWORD flOptions, SIZE_T dwInitialSize, SIZE_T dwMaximumSize)
+		inline CMemoryPool<ObjectType>* CMemoryPool<ObjectType>::GetInstance()
+		{
+			if (CMemoryPool<ObjectType>::pInstance == nullptr)
+			{
+				CMemoryPool<ObjectType>::pInstance = new CMemoryPool<ObjectType>(flOptions, dwInitialSize, dwMaximumSize);
+				atexit(CMemoryPool<ObjectType>::Destroy);
+			}
+
+			return CMemoryPool<ObjectType>::pInstance;
+		}
+
+		template<typename ObjectType>
+		inline void CMemoryPool<ObjectType>::Destroy()
+		{
+			delete CMemoryPool<ObjectType>::pInstance;
+			CMemoryPool<ObjectType>::pInstance = nullptr;
+		}
+
+		template<typename ObjectType>
+		CMemoryPool<ObjectType>::CMemoryPool(DWORD flOptions, SIZE_T dwInitialSize, SIZE_T dwMaximumSize)
 			: _Top(nullptr)
 		{
 			CMemoryPool<ObjectType>::_VerificationCode = reinterpret_cast<ULONG_PTR>(this);
@@ -56,16 +81,6 @@ namespace SJNET
 		template<typename ObjectType>
 		CMemoryPool<ObjectType>::~CMemoryPool()
 		{
-			Node* pDel = this->_Top;
-			Node* temp;
-
-			while (pDel)
-			{
-				temp = pDel->below;
-				delete pDel;
-				pDel = temp;
-			}
-
 			HeapDestroy(CMemoryPool<ObjectType>::_HeapHandle);
 		}
 
@@ -97,14 +112,15 @@ namespace SJNET
 			}
 		}
 
-		// 메모리 풀로 반환되는 객체는 무조건 소멸자가 호출된다.
+		// 메모리 풀로 반환되는 객체에는 무조건 소멸자 호출
 		template<typename ObjectType>
 		void CMemoryPool<ObjectType>::ReturnObjectToPool(ObjectType* _Ret)
 		{
 			if (reinterpret_cast<Node*>(_Ret)->verificationCode != this->_VerificationCode)
-				ReturnObjectErrorCrash(reinterpret_cast<Node*>(_Ret)->verificationCode);
+				CMemoryPool<ObjectType>::ReturnObjectErrorCrash(reinterpret_cast<Node*>(_Ret)->verificationCode);
 
 			_Ret->~ObjectType();
+
 			Node* prevTop = this->_Top;
 			this->_Top = reinterpret_cast<Node*>(_Ret);
 			this->_Top->below = prevTop;
@@ -113,21 +129,21 @@ namespace SJNET
 		template<typename ObjectType>
 		inline void CMemoryPool<ObjectType>::GetObjectErrorCrash()
 		{
-			SJNET::LIB::CFileLogger fLogger(L"CMemoryPool Error_", true);
+			SJNET::LIB::CFileLogger fLogger(L"CMemoryPool_MT Error_", true);
 			wchar_t logBuffer[128];
 			StringCbPrintfW(logBuffer, sizeof(logBuffer), L"[MEMORY POOL ERROR] std::bad_alloc exception occurred. Pool code is %llu", _VerificationCode);
 			fLogger.WriteLog(logBuffer, LogType::LT_CRITICAL);
-			ForceCrash(0xAABBCCDD);
+			ForceCrash(0xABABABAB);
 		}
 
 		template<typename ObjectType>
-		inline void CMemoryPool<ObjectType>::ReturnObjectErrorCrash(ULONG_PTR wrongVerificationCode)
+		inline void CMemoryPool<ObjectType>::ReturnObjectErrorCrash(ULONG_PTR llWrongVerificationCode)
 		{
 			SJNET::LIB::CFileLogger fLogger(L"CMemoryPool Error_", true);
 			wchar_t logBuffer[128];
-			StringCbPrintfW(logBuffer, sizeof(logBuffer), L"[MEMORY POOL ERROR] An object with wrong verification code %llu was returned to the memory pool of code %llu.", wrongVerificationCode, _VerificationCode);
+			StringCbPrintfW(logBuffer, sizeof(logBuffer), L"[MEMORY POOL ERROR] An object with wrong verification code %llu was returned to the memory pool of code %llu.", llWrongVerificationCode, _VerificationCode);
 			fLogger.WriteLog(logBuffer, LogType::LT_CRITICAL);
-			ForceCrash(0xABCDABCD);
+			ForceCrash(0xABABABAB);
 		}
 
 		template<typename ObjectType>
@@ -149,7 +165,22 @@ namespace SJNET
 		{
 			HeapFree(CMemoryPool<ObjectType>::_HeapHandle, 0, _Block);
 		}
+
+		// --------------------------------------------------------------------------------------
+		template<typename ObjectType>
+		CMemoryPool<ObjectType>* CMemoryPool<ObjectType>::pInstance = nullptr;
+
+		template<typename ObjectType>
+		ULONG_PTR CMemoryPool<ObjectType>::_VerificationCode = -1;
+
+		template<typename ObjectType>
+		HANDLE CMemoryPool<ObjectType>::_HeapHandle = NULL;
+
+		template<typename ObjectType>
+		DWORD CMemoryPool<ObjectType>::_HeapOptions = flOptions;
 	}
 }
 
 #pragma warning(default:26495)
+
+#undef _WINSOCKAPI_
